3d120b599a80677414fd6bd11b7b21def51bb870
Onda-dev <manuelaltieri3@gmail.com>
2022-06-23 15:39:24 +0200

    main ok

--- index.php
+++ index.php
@@ -57,15 +57,23 @@ $faqs = [
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
 </head>
 <body>
-    <ul>
-        <?php foreach($faqs as $faq) {?>
-            <li>
-                <h2><?= $faq["question"];?></h2>
-                <p><?= $faq["answer"];?></p>
-            </li>
-        <?php } ?>
-    </ul>
+    <main>
+        <div class="container">
+            <ul>
+                <?php foreach($faqs as $faq) {?>
+                    <li>
+                        <h2><?= $faq["question"];?></h2>
+                        <p><?= $faq["answer"];?></p>
+                    </li>
+                <?php } ?>
+            </ul>
+        </div>
+    </main>
 </body>
 </html>
